88d2fa4705630fd43699267439bb2486657682e8
Stephen Fudge <sfudge@gmail.com>
2022-06-27 19:08:42 -0400

    finished api routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,9 +1,133 @@
 // dependencies
 const router = require('express').Router();
-const { User, Post, Comment } = require('../../models');
+const {
+    User,
+    Post,
+    Comment
+} = require('../../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
 
-router.get('/', (req,res) => {
+// get all posts
+router.get('/', (req, res) => {
     Post.findAll({
-        
+            attributes: [
+                'id',
+                'title',
+                'post_content'
+            ],
+            include: [{
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }]
+        })
+        .then(postData => req.json(postData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// get post based on id
+router.get('/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'title',
+                'post_content'
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+        .then(postData => {
+            if (!postData) {
+                res.status(404).json({
+                    message: 'No post found with that ID'
+                });
+                return;
+            }
+            res.json(postData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// post with the auth.js shortcut
+router.post('/', withAuth, (req,res) => {
+    Post.create({
+        title: req.body.title,
+        post_content: req.body.post_content,
+        user_id: req.session.user_id
     })
-})
+    .then(postData => res.json(postData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// update given the id  with the auth.js shortcut
+router.put('/:id', withAuth, (req,res) => {
+    Post.update({
+        title: req.body.title,
+        post_content: req.body.post_content
+    },
+    {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(postData => {
+        if(!postData){
+            res.status(404).json({ message: 'No post found with that ID' });
+            return;
+        }
+        res.json(postData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// delete given the id
+router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(postData => {
+        if(!postData){
+            res.status(404).json({ message: 'No post found with that ID' });
+            return;
+        }
+        res.json(postData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router; 
--- server.js
+++ server.js
@@ -6,6 +6,26 @@ const path = require ('path');
 // const routes = require('./controllers');
 const hbs = exphbs.create({});
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Set up sessions with cookies
+const sess = {
+  secret: 'Super secret',
+  cookie: {
+    // Stored in milliseconds (86400 === 1 day)
+    maxAge: 86400,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+
 
 
 // Sets up the Express App
@@ -17,7 +37,7 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(require('./controllers/routes'));
+// app.use(routes);
 
 // starts the connection to the db and set the server to begin listening
 sequelize.sync({ force: false }).then(() => {
